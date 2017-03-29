762653aa12aecb302927ce662d89316e6e2dd0e9
David Houck <David.Houck@sas.com>
2017-03-29 15:34:35 -0400

    Add my-redis service to manifest and cf

--- 12f_app/src/main/java/com/github/dhoucgitter/app/StockSpringBootStarter.java
+++ 12f_app/src/main/java/com/github/dhoucgitter/app/StockSpringBootStarter.java
@@ -3,6 +3,7 @@ package com.github.dhoucgitter.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableRedisHttpSession
 @SpringBootApplication
 public class StockSpringBootStarter {
     public static void main(String[] args) {
