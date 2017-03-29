9705979918d97e0974a37915504e574f07ba5da9
David Houck <David.Houck@sas.com>
2017-03-29 16:05:46 -0400

    Add @AutoWired

--- 12f_app/src/main/java/com/github/dhoucgitter/app/EnvResource.java
+++ 12f_app/src/main/java/com/github/dhoucgitter/app/EnvResource.java
@@ -1,5 +1,6 @@
 package com.github.dhoucgitter.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EnvResource {
 
+	@Autowired
 	private Environment environment;
 	@RequestMapping(value = "/get-env", method = RequestMethod.GET)
 	public String getEnv(@RequestParam String env) {
